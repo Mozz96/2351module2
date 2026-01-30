eab48f04bd6e5a854556cb03fa10361b780e58bc
Jeremy Martinez <42855827+Mozz96@users.noreply.github.com>
2026-01-29 18:10:25 -0600

    v1
    initial version

---
+++ js/app.js
@@ -0,0 +1,38 @@
+
+
+// This variable is a global variable 
+{
+    var x = 12
+}
+
+
+// This variable is a block variable 
+
+{
+    let x = 13
+}
+
+
+// This code does a mathematical equation on two variables
+
+{
+    const number1 = 9;
+    const number2 = 10;
+
+    let equation = number1 + number2;
+}
+
+
+
+// This code is a string variable
+let fruitQuestion = "My favorite fruit is the pear, what about you?";
+
+// This code is an object variable
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue"
+};
+
+console.log(fruitQuestion)
